48f6de656fde76e5e53624c856a37e82172bd00a
pablocorks <pablomrx@gmail.com>
2024-11-26 08:49:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+document.getElementById("paciente").addEventListener("click", () => {
+  alert("Abrindo tela do Paciente...");
+  // Aqui você redirecionará para outra página ou seção
+});
+
+document.getElementById("medico").addEventListener("click", () => {
+  const senha = prompt("Digite a senha para acessar:");
+  if (senha === "1803") {
+    alert("Abrindo tela do Médico...");
+    // Aqui você redirecionará para outra página ou seção
+  } else {
+    alert("Senha incorreta!");
+  }
+});
+
+document.getElementById("secretaria").addEventListener("click", () => {
+  const senha = prompt("Digite a senha para acessar:");
+  if (senha === "1803") {
+    alert("Abrindo tela da Secretária...");
+    // Aqui você redirecionará para outra página ou seção
+  } else {
+    alert("Senha incorreta!");
+  }
+});
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("service-worker.js")
